83ab39dbaaf58badbf0c35053a0d2ac4c9fc0b88
capezza <christian.capezza@gmail.com>
2019-04-22 20:15:04 +0100

    Added derivatives wrt additional parameters theta

--- R/convertDerivStackPositive.R
+++ R/convertDerivStackPositive.R
@@ -6,53 +6,48 @@
 #' the non linear predictor eta into derivatives with respect to the
 #' regression coefficients and smoothing parameters
 #'
-#' @param param regression coefficients
+#' @param beta regression coefficients
 #' @param theta vector of other parameters of the response variable distribution (to be included in beta to be estimated by ML)
 #' @param X nxK model matrix of the convex combination
 #' @param Z nxp model matrix
 #' @param le nx1 vector of 1st derivatives of the log-likelihood with respect to eta
 #' @param lee nx1 vector of 2nd derivatives of the log-likelihood with respect to eta
 #' @param leee nx1 vector of 3rd derivatives of the log-likelihood with respect to eta
-#' @param d1b (p(K-1))xm matrix of derivatives of the Hessian with respect to the smoothing parameters (at the moment only one smoothing parameter)
+#' @param d1b (pK)xm matrix of derivatives of the Hessian with respect to the smoothing parameters
 #' @param deriv 0: just grad and Hess, 1: first deriv of Hess
 #'
 #' @name convertDerivStackPositive
 #' @rdname convertDerivStackPositive
 #' @export
 #' 
-convertDerivStackPositive <- function(param, theta, X, Z, le, lee, leee = 0, 
-                              d1b = 0, deriv = 0) {
+convertDerivStackPositive <- function(beta, X, Z,
+                                      le, lt, 
+                                      lee, let, ltt, 
+                                      leee = 0, leet = 0, lett = 0, lttt = 0, 
+                                      d1b = 0, deriv = 0) {
   
   d1H <- NULL ## default
   
-  p <- ncol(Z); n <- nrow(X); K <- ncol(X)
-  param <- matrix(param, nrow = p, ncol = K)
+  p <- ncol(Z); n <- nrow(X); K <- ncol(X); nth <- length(lt)
+  beta <- matrix(beta, nrow = p, ncol = K)
   P <- p * K
-  nu <- Z %*% param
+  nu <- Z %*% beta
   a <- exp(nu)
   eta <- rowSums(a * X)
   
   ## the gradient...
-  en <- X * exp(nu)
+  en <- X * a
   ln <- le * en
-  lb <- as.vector(t(Z) %*% ln)
+  lb <- c(as.vector(t(Z) %*% ln), lt)
   
   ## the Hessian...
-  enn <- list()
-  coun <- 1
-  for (jj in 1:K) for (kk in jj:K) {
-    if (jj == kk) {
-      enn[[coun]] <- exp(nu[, jj]) * X[, jj] 
-    } else enn[[coun]] <- rep(0, n)
-    coun <- coun + 1
-  }
-  enn <- do.call(cbind, enn)
   i2 <- mgcv::trind.generator(K)$i2
   lnn <- list()
-  coun <- 1
+  coun <- 0
   for (rr in 1:K) for (ss in rr:K) {
-    lnn[[coun]] <- lee * en[, rr] * en[, ss] + le * enn[, i2[rr, ss]]
     coun <- coun + 1
+    lnn[[coun]] <- lee * en[, rr] * en[, ss]
+    if (rr == ss) lnn[[coun]] <- lnn[[coun]] + le * en[, rr] # note enn = en
   }
   lnn <- do.call(cbind, lnn)
   lbb <- lapply(1:K, function(x) vector("list", K))
@@ -62,69 +57,112 @@ convertDerivStackPositive <- function(param, theta, X, Z, le, lee, leee = 0,
   }
   lbb <- lapply(lbb, function (x) do.call(rbind, x))
   lbb <- matrix(do.call(cbind, lbb), nrow = P)
+  ltb <- list()
+  coun <- 0
+  for (rr in 1:K) {
+    coun <- coun + 1
+    ltb[[coun]] <- t(let) %*% (en[, rr] * Z)
+  }
+  ltb <- do.call(cbind, ltb)
+  lbb <- rbind(cbind(lbb, t(ltb)), cbind(ltb, ltt))
   
-  if (deriv == 1) {
-    ennn <- list()
-    coun <- 1
-    for (jj in 1:K) for (kk in jj:K) for (ll in kk:K) {
-      if(jj == kk & kk == ll) {
-        ennn[[coun]] <- exp(nu[, jj]) * X[, jj] 
-      } else ennn[[coun]] <- rep(0, n)
-      coun <- coun + 1
-    }
-    ennn <- do.call(cbind, ennn)
+  if (deriv == 1) { ## only store diagonal of d1H
     
-    i3 <- mgcv::trind.generator(K)$i3
+    stop("diagonal elements of d1H only not provided, only full d1H implemented")
+    
+  } ## if deriv == 1
+  
+  if (deriv == 2) { ## store full d1H
+    
+    # ennn <- list()
+    # coun <- 0
+    # for (jj in 1:K) for (kk in jj:K) for (ll in kk:K) {
+    #   coun <- coun + 1
+    #   if(jj == kk & kk == ll) {
+    #     ennn[[coun]] <- en[, jj]
+    #   } else ennn[[coun]] <- rep(0, n)
+    # }
+    # ennn <- do.call(cbind, ennn)
+    
     lnnn <- list()
-    coun <- 1
+    coun <- 0
     for (rr in 1:K) for (ss in rr:K) for (tt in ss:K) {
-      lnnn[[coun]] <- leee * en[, rr] * en[, ss] * en[, tt] +
-        lee * (enn[, i2[rr, tt]] * en[, ss] + 
-                 enn[, i2[ss, tt]] * en[, rr] + 
-                 enn[, i2[rr, ss]] * en[, tt]) +
-        le * ennn[, i3[rr, ss, tt]]
       coun <- coun + 1
+      term1 <- leee * en[, rr] * en[, ss] * en[, tt]
+      term2 <- term3 <- 0
+      if (rr == ss) term2 <- term2 + en[, rr] * en[, tt]
+      if (rr == tt) term2 <- term2 + en[, rr] * en[, ss]
+      if (ss == tt) term2 <- term2 + en[, ss] * en[, rr]
+      term2 <- lee * term2
+      if (rr == ss & ss == tt) term3 <- le * en[, rr] # note ennn = enn = en
+      lnnn[[coun]] <- term1 + term2 + term3
+        
+    # i3 <- mgcv::trind.generator(K)$i3
+    #   lnnn[[coun]] <- leee * en[, rr] * en[, ss] * en[, tt] +
+    #     lee * (enn[, i2[rr, tt]] * en[, ss] + 
+    #              enn[, i2[ss, tt]] * en[, rr] + 
+    #              enn[, i2[rr, ss]] * en[, tt]) +
+    #     le * ennn[, i3[rr, ss, tt]]
+      
     }
     lnnn <- do.call(cbind, lnnn)
     
-    d1H <- lapply(1:K, function(x) vector("list", K))
-    for (rr in 1:K) for (ss in rr:K) {
-      d1H[[rr]][[ss]] <- t(Z) %*% (Z * rowSums((Z %*% matrix(d1b, nrow = p, ncol = K)) * 
-                                                 lnnn[, i3[rr, ss, 1:K]]))
-      if (ss > rr) d1H[[ss]][[rr]] <- d1H[[rr]][[ss]]
-    }
-    d1H <- lapply(d1H, function (x) do.call(rbind, x))
-    d1H <- matrix(do.call(cbind, d1H), nrow = P)
-    d1H <- as.numeric(d1H)
+    m <- ncol(d1b)
+    d1br <- d1b[1:(p * K), , drop = FALSE]
+    d1tr <- d1b[(nrow(d1b) - nth + 1):nrow(d1b), , drop = FALSE]
     
-    # The following calculates the third derivatives wrt regression coefficients
-    # We do not implement it because it should not be required by gam.fit5
-    # lbbb <- lapply(1:K, function(x) {
-    #   lapply(1:K, function(y) vector("list", K))
-    # })
-    # for (rr in 1:K) for (ss in 1:K) for (tt in 1:K) {
-    #   lbbb[[rr]][[ss]][[tt]] <- array(0, dim = c(p, p, p))
-    #   for (jj in 1:p) for (kk in 1:p) for (ll in 1:p) {
-    #     lbbb[[rr]][[ss]][[tt]][jj, kk, ll] <- 
-    #       sum(lnnn[, i3[rr, ss, tt]] * Z[, jj] * Z[, kk] * Z[, ll])
-    #   }
-    # }
-    # for (rr in 1:K) for (ss in 1:K) {
-    #   lbbb[[rr]][[ss]] <- do.call(abind, list(lbbb[[rr]][[ss]], along = 3))
-    # }
-    # for (rr in 1:K) {
-    #   lbbb[[rr]] <- do.call(abind, list(lbbb[[rr]], along = 2))
-    # }
-    # lbbb <- do.call(abind, list(lbbb, along = 1))
-    # 
-    # lbbbMat <- list()
-    # coun <- 1
-    # for (jj in 1:P) for (kk in jj:P) for (ll in kk:P) {
-    #   lbbbMat[[coun]] <- lbbb[jj, kk, ll]
-    #   coun <- coun + 1
-    # }
-    # l3 <- do.call(c, lbbbMat)
-  }
+    d1H <- list()
+    i2 <- trind.generator(nth)$i2
+    for (l in 1:m) {
+      
+      d1brl <- matrix(d1br[, l], nrow = p, ncol = K)
+      
+      i3 <- trind.generator(K)$i3
+      lbbr <- lbbt <- lapply(1:K, function(x) vector("list", K))
+      lbbt <- lapply(1:nth, function(x) lbbr)
+      for (rr in 1:K) for (ss in rr:K) {
+        
+        A <- matrix(leet * en[, rr] * en[, ss])
+        if (rr == ss) A <- A + let * en[, rr] 
+        
+        V1 <- rowSums((Z %*% d1brl) * lnnn[, i3[rr, ss, ]])
+        V2 <- A %*% d1tr[, l, drop = FALSE]
+        V <- as.numeric(V1 + V2)
+        lbbr[[rr]][[ss]] <- t(Z) %*% (Z * V)
+        if (ss > rr) lbbr[[ss]][[rr]] <- lbbr[[rr]][[ss]]
+        
+        for (uu in 1:nth) {
+          lbbt[[uu]][[rr]][[ss]] <- t(Z) %*% (Z * A[, uu])
+          if (ss > rr) lbbt[[uu]][[ss]][[rr]] <- lbbt[[uu]][[rr]][[ss]]
+        }
+      }
+      lbbr <- lapply(lbbr, function (x) do.call(rbind, x))
+      lbbr <- matrix(do.call(cbind, lbbr), nrow = P)
+      
+      lbtr <- list()
+      coun <- 0
+      for (uu in 1:nth) {
+        coun <- coun + 1
+        lbbt[[uu]] <- lapply(lbbt[[uu]], function (x) do.call(rbind, x))
+        lbbt[[uu]] <- matrix(do.call(cbind, lbbt[[uu]]), nrow = P)
+        lbtr[[uu]] <- lbbt[[uu]] %*% as.vector(d1brl) 
+        lbtr[[uu]] <- lbtr[[uu]] + as.numeric(t(Z) %*% (
+          as.numeric(lett[, i2[uu, 1:nth], drop = FALSE] %*% d1tr[, l]) * en))
+        uu <- uu + 1
+      }
+      lbtr <- do.call(cbind, lbtr)
+      
+      lttr <- matrix(0, nrow = nth, ncol = nth)
+      for (uu in 1:nth) for(vv in uu:nth) {
+        lttr[uu, vv] <- sum(t(Z) %*% (en * lett[, i2[uu, vv]]) * d1brl) + 
+          lttt[uu, vv, ] %*% d1tr[, l]
+        if (vv > uu) lttr[vv, uu] <- lttr[uu, vv]
+      }
+      
+      d1H[[l]] <- rbind(cbind(lbbr, lbtr), cbind(t(lbtr), lttr))
+      
+    } ## for (l in 1:m)
+  } ## if deriv == 2
   
   list(lb=lb,lbb=lbb,d1H=d1H)
 }
