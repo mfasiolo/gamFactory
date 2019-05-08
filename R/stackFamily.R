57fb5c3f687a28929cf1eddd18fb489d80074e9a
capezza <christian.capezza@gmail.com>
2019-05-08 17:53:01 +0100

    fixed error in stackFamily.R

--- R/stackFamily.R
+++ R/stackFamily.R
@@ -96,7 +96,7 @@ stackFamily <- function(X, familyDeriv, link="identity") {
       G$family$ibeta[lpi[[k]]] <- um$b
     }
     fit <- G$X %*% G$family$ibeta
-    res <- y - fit
+    res <- G$y - fit
     
     G$family$ibeta[nbeta + 1:ntheta] <- familyDeriv$ml(res)[- 1]
     
