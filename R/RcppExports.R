c9a99a23757ab579c7ac30d0e07e5b539098f123
capezza <christian.capezza@gmail.com>
2019-08-08 15:44:59 +0100

    fixed error when calling Rcpp functions

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 lbbMult <- function(x, lpstart, lpend, lnn, i2) {
-    .Call('_gamFactory_lbbMult', PACKAGE = 'gamFactory', x, lpstart, lpend, lnn, i2)
+    .Call('_gamFactory_lbbMult', x, lpstart, lpend, lnn, i2)
 }
 
