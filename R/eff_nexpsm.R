839a972dd7d9159db14b9805a90a6d52e2216514
Matteo Fasiolo <mf15002@it031161.wks.bris.ac.uk>
2022-03-04 15:57:48 +0000

    Streamlines eff()$eval for general nested effects

--- R/eff_nexpsm.R
+++ R/eff_nexpsm.R
@@ -9,49 +9,14 @@ eff_nexpsm <- function(y, Xi, basis, x0 = NULL){
   
   force(y); force(Xi); force(basis); force(x0)
   
-  eval <- function(param, deriv = 0){
-    
-    na <- ncol( Xi ) + 1
-    nb <- length(param) - na 
-    
-    # Scale, inner and outer coefficients
-    a0 <- exp( param[1] )
-    alpha <- param[ 2:na ]
-    beta <- param[ -(1:na) ]
-    
-    inner <- expsmooth(y = y, Xi = Xi, beta = alpha, x0 = x0, deriv = deriv)
-
-    # Build P-spline basis and its derivatives
-    # We are also getting the derivatives of the inner linear predictor w.r.t.
-    # the scale parameter a0 and the coefficients alpha
-    store <- basis(x = a0 * (inner$d0 - mean(inner$d0)), deriv = deriv)
-    store$g <- a0 * (inner$d0 - mean(inner$d0)) 
-    store$Xi <- Xi
-    if( deriv >= 1 ){
-      store$f1 <- drop( store$X1 %*% beta )
-      store$g1 <-  cbind(store$g, a0 * t(t(inner$d1) - colMeans(inner$d1)))
-      if( deriv >= 2 ){
-        store$f2 <- drop( store$X2 %*% beta )
-        store$g2 <- cbind(store$g1, a0 * t(t(inner$d2) - colMeans(inner$d2)))
-        if( deriv >= 3 ){
-          store$f3 <- drop( store$X3 %*% beta )
-          store$g3 <- cbind(store$g2, a0 * t(t(inner$d3) - colMeans(inner$d3)))
-        }
-      }
-    }
-    
-    o <- eff_nexpsm(y = y, Xi = Xi, basis = basis, x0 = x0)
-    o$f <- drop( store$X0 %*% beta )
-    o$param <- param
-    o$na <- na
-    o$store <- store
-    o$deriv <- deriv
-    
-    return( o )
-    
-  }
+  incall <- as.expression(quote(do.call("expsmooth", list("y" = y, "Xi" = Xi, "beta" = alpha, "deriv" = deriv), quote = TRUE)))
+  efcall <- as.expression(quote(do.call("eff_nexpsm", list("y" = y, "Xi" = Xi, "basis" = basis, "x0" = x0), quote = TRUE)))
   
-  out <- structure(list("eval" = eval), class = c("nexpsm", "nested"))
+  .eval <- .get_eff_eval()
+  
+  environment(.eval) <- as.environment(environment())
+  
+  out <- structure(list("eval" = .eval), class = c("nexpsm", "nested"))
   
   return( out )
   
