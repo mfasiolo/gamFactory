89ea6c168583662c8e2b988b29edd67226016d25
Matteo Fasiolo <mf15002@it031161.wks.bris.ac.uk>
2023-06-13 15:53:08 +0100

    Reparametrisation matrix B was inverted!!!

--- R/I_getBmatrix.R
+++ R/I_getBmatrix.R
@@ -1,18 +1,18 @@
 #################
 # Reparametrisation matrix to work with diagonal penalty
-# 
+#
 # r is rank of penalty matrix P
 .getBmatrix <- function(P, r){
-  
+
   d <- ncol( P )
   ei <- eigen( P )
   v <- ei$values
-  
+
   if(r < d){
     v[ (r+1):d ] <- 1
   }
-  
-  B <- diag(sqrt(v), d) %*% t(ei$vectors)
-  
+
+  B <- ei$vectors %*% diag(1/sqrt(v), d)
+
   return( B )
 }
