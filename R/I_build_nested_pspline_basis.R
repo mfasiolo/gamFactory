d48052b307d971c65c96c0ba7b80c078cb7c4232
Matteo Fasiolo <mf15002@it031161.wks.bris.ac.uk>
2024-03-04 15:47:08 +0000

    Change sum-to-zero constraints and added slope separately.

--- R/I_build_nested_pspline_basis.R
+++ R/I_build_nested_pspline_basis.R
@@ -9,6 +9,10 @@
   x <- data[[term]]
   knots_x <- knots[[term]] 
   
+  if( abs(mean(x)) > 1e-6 ){
+    stop("The data should be centered!")
+  }
+  
   m <- object$p.order
   if(length(m) > 2){
     m <- object$p.order <-  m[1:2]
@@ -41,18 +45,40 @@
   }
   
   n <- nrow(out$X)
-  si$xseq <- seq(basis$krange[1], basis$krange[2], length.out = n)
-
-  # Outer smooth effect is not centered, so we impose that sum_i f(x_i) = 0 were x_i are from uniform grid xseq.
+ 
+  # Outer smooth effect is not centered, so we impose that f(0) = 0, i.e. a point constraint at the origin.
   # We also impose, for j = 1 and 2, that f''(krange[j]) = f'''(krange[j]) = f''''(krange[j]) =  0. So in total we have 7 constraints.
-  # We need to a) create model matrix X0 corresponding to x_i's on uniform grid xseq 
-  #            b) find null space (NS) corresponding to constraints, to impose the constraints via reparametrisation
+  # If the order of derivative penalty >= 2, we also orthogonalise B-spline bases wrt linear basis (straight line). So 8 contraints in this case.
+  # We need to a) find null space (NS) corresponding to constraints, to impose the constraints via reparametrisation
+  #            b) if add_slope == TRUE we do some adjustment to "separate" linear basis from the rest 
   #            c) project original model matrix X on NS and reparametrise S
   #            d) reduce bs.dim by the number of constraints
   #            e) numerically calculate rank of S (I don't know how to get it analytically)
-  Xtmp1 <- basis$evalX(si$xseq)$X0
+  X00 <- basis$evalX(x = 0)
   Xth <- basis$evalX(x = basis$krange, deriv = 4)
-  NS <- Null( t(rbind(colMeans(Xtmp1), Xth$X2, Xth$X3, Xth$X4)) )
+  con <- rbind(X00$X0, Xth$X2, Xth$X3, Xth$X4)
+  NS <- Null(t(con))
+  
+  # We add constrain to remove linear basis from first "dsmo" columns, but then 
+  # we add an unpenalised linear basis in the last column of the model matrix.
+  # The NS re-parametrisation matrix is padded with 0s and (a single) 1 to make sure linear
+  # effect is in last column of reparametrised X. The S penalty is padded with 0s to
+  # make sure that the linear basis is unpenalised. We add to Xth the value of the
+  # derivatives of the linear basis at the extreme knots.
+  add_slope <- m[2] >= 2
+  if(add_slope){
+    con <- rbind(con, t(x)%*%out$X)
+    NS <- Null(t(con))
+    NS <- rbind(cbind(NS, rep(0, nrow(NS))), c(rep(0, ncol(NS)), 1))
+    out$X <- cbind(out$X, x)
+    out$S[[1]] <- rbind(cbind(out$S[[1]], 0), 0)
+    out$bs.dim <- out$bs.dim + 1
+    Xth$X0 <- cbind(Xth$X0, basis$krange)
+    Xth$X1 <- cbind(Xth$X1, 1)
+    Xth$X2 <- cbind(Xth$X2, 0)
+    Xth$X3 <- cbind(Xth$X3, 0)
+    Xth$X4 <- cbind(Xth$X4, 0)
+  }
   out$X <- out$X %*% NS
   out$S[[1]] <- t(NS) %*% out$S[[1]] %*% NS 
   ncon <- nrow(NS) - ncol(NS)
@@ -61,8 +87,17 @@
   out$rank <- rankMatrix(out$S[[1]])
   
   # Linearly extrapolate outer B-spline basis beyond inner knots (those at which we imposed the constraints above)
+  # NOTE method = "smooth" won't work if add_slope == TRUE
   out$X <- linextr(x = x, b = list(X0 = out$X), th = basis$krange, 
                    Xbo = Xth$X0%*%NS, Xbo1 = Xth$X1%*%NS, method = "simple")$X0 # If you change "simple" here, you need to do it also elsewhere!!
+  
+  # To aid identifiability we make penalty on the outer smooth full rank
+  if(out$rank < dsmo){
+    eig <- eigen(out$S[[1]])
+    eig$values[-(1:out$rank)] <- pmin(eig$values[1] * 1e-5, eig$values[out$rank])
+    out$S[[1]] <- eig$vectors %*% diag(eig$values) %*% t(eig$vectors)
+    out$rank <- ncol(out$X)
+  }
   
   # Reparametrise the outer smooth so that penalty is diagonal (we need diagonal penalty otherwise mgcv will
   # reparametrise again with Sl.repara)
@@ -92,6 +127,6 @@
   
   # Store extra things needed later on. 
   out$xt$si <- si
-  out$xt$basis <- .wrap_nested_basis(b = basis, P = NS %*% sm$B, Xth = Xth)
+  out$xt$basis <- .wrap_nested_basis(b = basis, P = NS %*% sm$B, Xth = Xth, add_slope = add_slope)
   return(out)
 }
