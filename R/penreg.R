13a46212ecd53b7422dd9a417412f7a5104c89dd
Matteo Fasiolo <mf15002@it031161.wks.bris.ac.uk>
2026-06-12 11:07:36 +0100

    Align with mgcv 1.9.4

--- R/penreg.R
+++ R/penreg.R
@@ -13,7 +13,7 @@ penreg <- function(x,e,y) {
   ## Taken from Simon N. Wood (in mgcv it is "pen.reg")
   ## get coefficients of penalized regression of y on matrix x
   ## where e is a square root penalty. Idea is to use e mainly for 
-  ## regularization, so that edf is close to rank of x. 
+  ## regularization, so that edf is close to rank of x.  
   if (sum(abs(e))==0) { ## no penalization - easy
     b <- qr.coef(qr(x),y)
     b[!is.finite(b)] <- 0
@@ -30,20 +30,22 @@ penreg <- function(x,e,y) {
   ## now we want estimates with penalty weight low enough 
   ## EDF is k * rr where k is somewhere in e.g. (.7,.9)
   k <- .01 * norm(R)/norm(e)
-  qrr <- qr(rbind(R, e*k))
-  edf <- sum(qr.Q(qrr)[1:r, ]^2) 
-  re <- min(sum(colSums(abs(e))!=0), nrow(e)) ## NEW
-  re <- re - (Rrank(qr.R(qrr)) - rr) ## Matteo: subtract from rank(e) the number of directions 
-  ##         in e that are in null_space(X)
-  while (edf > rr-0.1*re) { ## increase penalization ## NEW
+  qrr <- qr(rbind(R, e*k));
+  edf <- sum(qr.Q(qrr)[1:r, ]^2)
+  ## compute rank of e less rank of the space penalized by e not in
+  ## range space of x, this is how much penalty can in principle change
+  ## edf by. Needed for corner cases where e.g. penalty is imposed for
+  ## identifiability reasons and then only penalizes null space of x...
+  re <- min(sum(colSums(abs(e))!=0),nrow(e)) - Rrank(qr.R(qrr)) + rr
+  while (edf > rr-.1*re) { ## increase penalization
     k <- k*10
-    qrr <- qr(rbind(R,e*k))
+    qrr <- qr(rbind(R,e*k));
     edf <- sum(qr.Q(qrr)[1:r,]^2)
-  }
-  while (edf<0.7*rr) { ## reduce penalization
-    k <- k/20
-    qrr <- qr(rbind(R,e*k))
-    edf <- sum(qr.Q(qrr)[1:r,]^2)
+  } 
+  while (edf<0.85*rr) { ## reduce penalization (was 0.7)
+    k <- k/5 ## was 20! 
+    qrr <- qr(rbind(R, e*k));
+    edf <- sum(qr.Q(qrr)[1:r, ]^2)
   } 
   b <- qr.coef(qrr,c(Qy,rep(0,nrow(e))))
   b[!is.finite(b)] <- 0
